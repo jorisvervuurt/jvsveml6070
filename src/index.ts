c80d5ea4fa3134ae646d12443e4f93a6d0ccdee7
Joris Vervuurt <developer@joris-vervuurt.com>
2020-12-29 14:14:20 +0100

    Remove logic leftover from testing.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import { IntegrationTime } from './lib/enums/IntegrationTime';
 import { Sensor } from './lib/Sensor';
+import { ShutdownMode } from './lib/enums/ShutdownMode';
 
 export {
     IntegrationTime,
     Sensor,
+    ShutdownMode,
 };
-
-// TODO: remove as this is for testing purposes.
-Sensor.init().then((sensor: Sensor) => {
-    console.log(sensor);
-});
