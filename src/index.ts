e1bab679e016837c2c51d76130648cbca705f941
Joris Vervuurt <developer@joris-vervuurt.com>
2021-01-01 10:21:34 +0100

    Update export logic.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export * as Enums from './lib/enums/exports';
 export * as Errors from './lib/errors/exports';
 export * as Helpers from './lib/helpers';
-export * from './lib/exports';
+
+export { IntegrationTime } from './lib/IntegrationTime';
+export { Sensor } from './lib/Sensor';
+export { SensorValue } from './lib/SensorValue';
+export { UvIndex } from './lib/UvIndex';
